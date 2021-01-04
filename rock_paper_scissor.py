62ac311088fb7a7761d32ba8222666a7159e0698
Jon Jos <jon.jos.dev@gmail.com>
2021-01-04 23:02:49 +0530

    finished rps

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -1,28 +1,29 @@
-import random 
+from random import randint
 
 l = ['rock','paper','scissor']
-
-computer = l[randint(0,2)]
-player = input("Enter rock/paper/scissor")
-
-
+flag = True
 while(flag):
+  computer = l[randint(0,2)]
+  player = input("Enter rock/paper/scissor:\t")
+  print("Computer : \t",computer)
+  print("Player : \t",player)
   if computer == player:
     print("A tie")
   elif computer == 'rock' and player == 'paper':
     print("Player Won!")
   elif computer == 'paper' and player == 'rock':
-    print("Computer Won!)
+    print("Computer Won!")
   elif computer == 'paper' and player == 'scissor':
-    print("Player Won!)
+    print("Player Won!")
   elif computer == 'scissor' and player == 'paper':
     print("Computer Won!")
   elif computer == 'rock' and player == 'scissor':
-    print("Computer Won!)
+    print("Computer Won!")
   else:
     print("Player Won!")
-  n = input("Do you wanna hit more?Y/N)
-  if n == Y and n == y:
+  
+  N = str(input("Do you wanna hit more?Y/N:\t"))
+  if N == 'Y' or N == 'y':
     flag=True
   else:
     flag=False
