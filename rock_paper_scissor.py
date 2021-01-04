e008485b137b5364bece236b3ae5b44026c8268b
John <71092045+Jon-Jos@users.noreply.github.com>
2021-01-04 22:22:20 +0530

    Create rock_paper_scissor.py

---
+++ rock_paper_scissor.py
@@ -0,0 +1,30 @@
+import random 
+
+l = ['rock','paper','scissor']
+
+computer = l[randint(0,2)]
+player = input("Enter rock/paper/scissor")
+
+
+while(flag):
+  if computer == player:
+    print("A tie")
+  elif computer == 'rock' and player == 'paper':
+    print("Player Won!")
+  elif computer == 'paper' and player == 'rock':
+    print("Computer Won!)
+  elif computer == 'paper' and player == 'scissor':
+    print("Player Won!)
+  elif computer == 'scissor' and player == 'paper':
+    print("Computer Won!")
+  elif computer == 'rock' and player == 'scissor':
+    print("Computer Won!)
+  else:
+    print("Player Won!")
+ 
+
+  
+ 
+
+
+
